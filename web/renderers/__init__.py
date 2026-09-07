eacd91942948082b5a4ba3234f34e86f4fd210ca
appsyndroms <fazzious@hotmail.com>
2026-09-07 07:51:12 +0200

    Create __init__.py

---
+++ web/renderers/__init__.py
@@ -0,0 +1,36 @@
+"""
+Renderingsmoduler för Fiskabilars statiska webbplats.
+"""
+
+from .findings import (
+    render_findings,
+    render_price_reductions,
+)
+
+from .history import (
+    render_market_history,
+)
+
+from .ml import (
+    render_ml,
+)
+
+from .outcomes import (
+    render_outcomes,
+    render_score_analysis,
+)
+
+from .page import (
+    build_html,
+)
+
+
+__all__ = [
+    "build_html",
+    "render_findings",
+    "render_price_reductions",
+    "render_outcomes",
+    "render_score_analysis",
+    "render_market_history",
+    "render_ml",
+]
